7fe9c8e0743a54e874eb3d8e032f6f998e14070d
George Yu <yudumpling@gmail.com>
2020-04-07 01:10:25 +0800

    Add support for instance config

--- ykps2020/__init__.py
+++ ykps2020/__init__.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_caching import Cache
 
-from config import app_config
+from instance.secrets import _CONFIG_OBJ
 
 
 # Initialize flask instances
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object(app_config['development'])
+app.config.from_object(_CONFIG_OBJ)
 app.config.from_pyfile('secrets.py')
 
 db = SQLAlchemy()
